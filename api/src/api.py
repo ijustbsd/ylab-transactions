915681d631204028c51c4e6576201791afe066be
Sergey Baboshin <ijustbsd@gmail.com>
2020-01-28 15:28:26 +0300

    Add add_user method to API

--- api/src/api.py
+++ api/src/api.py
@@ -1,16 +1,63 @@
 # coding: utf-8
 
+import base64
+import hashlib
+
+import bcrypt
+
 
 class API():
+    def __init__(self, pool):
+        self.pool = pool
 
-    async def add_user(self, **kwargs):
-        return 200, 'OK', {}
+    async def _get_user(self, email):
+        """
+        Получает данные о бользователе из бд.
+        """
+        async with self.pool.acquire() as conn:
+            async with conn.transaction():
+                user = await conn.fetchrow(
+                    "SELECT * FROM Users WHERE email = $1", email
+                )
+                return dict(user) if user else {}
 
-    async def auth(self, **kwargs):
+    async def add_user(self, email, password, balance, currency):
+        """
+        Добавляет нового пользователя в бд.
+        """
+
+        # проверяем зарегистрирован ли пользователь с таким email'ом
+        if await self._get_user(email):
+            return 400, 'User with this email already exist!', {}
+
+        # bcrypt не умеет работать со строками больше 72 символов
+        sha256_hash = hashlib.sha256(password.encode()).digest()
+        # защищаемся от NULL байтов
+        base64_hash = base64.b64encode(sha256_hash)
+        # хешируем и солим пароль для хранения в базе
+        hashed = bcrypt.hashpw(base64_hash, bcrypt.gensalt(12))
+
+        # добавляем пользователя в бд
+        async with self.pool.acquire() as conn:
+            async with conn.transaction():
+                await conn.execute("INSERT INTO Users VALUES ($1, $2, $3, $4)",
+                                   email, hashed, balance, currency)
+                return 200, 'OK', {}
+
+    async def auth(self, email, password):
+        """
+        Авторизует пользователя.
+        """
         return 200, 'OK', {}
 
     async def get_transactions_list(self, **kwargs):
+        """
+        Возвращает список всех операций по счёту пользователя.
+        """
         return 200, 'OK', {}
 
     async def make_transaction(self, **kwargs):
+        """
+        Переводит средства на счёт другого пользователя.
+        """
         return 200, 'OK', {}
--- api/src/main.py
+++ api/src/main.py
@@ -1,17 +1,13 @@
 # coding: utf-8
 
 import json
-import logging
+import os
 
+import asyncpg
 from aiohttp import web
 from jsonschema import ValidationError, validate
 
 from api import API
-
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-
-api = API()
 
 routes = web.RouteTableDef()
 
@@ -34,21 +30,35 @@ def api_response(code, msg, payload=None):
 async def main_post(request):
     try:
         request_data = await request.json()
+        # валидируем данные согласно jsonschem'е
         validate(request_data, json_schema)
     except json.decoder.JSONDecodeError:
         return api_response(400, 'Invalid JSON!')
     except ValidationError as e:
         return api_response(400, e.message)
 
-    (query, params), = request_data.pop('request').items()
-    params.update(request_data)
-
-    code, msg, payload = await getattr(api, query)(**params)
+    # если данные прошли валидацию, то достаём из них поле с запросом
+    (query, params), = request_data['request'].items()
+    # вызываем метод используя getattr т.к. он провалидирован jsonschem'ой
+    code, msg, payload = await getattr(request.app['api'], query)(**params)
     return api_response(code, msg, payload)
 
 
 async def create_app():
     app = web.Application()
+    app['pool'] = await asyncpg.create_pool(
+        host='postgres',
+        user=os.environ['DB_USER'],
+        password=os.environ['DB_PASSWORD'],
+        database=os.environ['DB_NAME']
+    )
+    app['api'] = API(pool=app['pool'])
+    # создаём первого пользователя, который сможет добавлять последующих
+    email = os.environ['USER_EMAIL']
+    password = os.environ['USER_PASSWORD']
+    balance = os.environ['USER_BALANCE']
+    currency = os.environ['USER_CURRENCY']
+    await app['api'].add_user(email, password, balance, currency)
     app.add_routes(routes)
     return app
 
